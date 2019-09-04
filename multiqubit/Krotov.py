4790a02516e02dd572e20241ddbc257ce3269fa0
93xiaoming <39090764+93xiaoming@users.noreply.github.com>
2019-09-04 09:48:41 +0800

    Add files via upload

---
+++ multiqubit/Krotov.py
@@ -0,0 +1,62 @@
+import numpy as np
+from scipy import linalg
+
+
+def hamiltonian(j):
+    j=j.tolist()
+    dim=len(j)
+    J  = 1.
+    H  = np.diag([1. for ii in range(dim-1)],1)+np.diag([1. for ii in range(dim-1)],-1)
+    H += J*np.diag(j)
+    return H
+
+
+dim=8
+N = 20
+T = np.pi*(dim-1)
+dt = T/N
+ep_max = 500 
+
+observable = np.mat(np.zeros(shape=(dim,dim), dtype=complex))
+observable[-1, -1] = 1
+
+psi = np.mat(np.zeros(shape=(dim, N+1), dtype=complex))
+psi[0,0] = 1    
+pseudo = np.mat(np.zeros(shape=(dim, N+1), dtype=complex))    # 
+
+seq = 40*np.random.rand(N,dim)
+seq_f = seq
+
+for i in range(N):
+    psi[:,i+1] = linalg.expm(-(1j) * hamiltonian(seq[i]) * dt).dot(psi[:,i])
+pseudo[:,-1] = observable.dot(psi[:,-1])
+
+dx = 0.01   
+fid_max=0
+for episode in range(ep_max):
+    for j in reversed(range(N)):
+        pseudo[:,j] = linalg.expm((1j) * hamiltonian(seq[j]) * dt).dot(pseudo[:,j+1])
+    for k in range(N):
+        for ii in range(dim):
+            seq_f[k,ii] = seq[k,ii] + 1*(pseudo[ii,k].conj()* psi[ii,k]).imag
+        
+        seq_f = np.clip(seq_f,0,40)
+        psi[:,k+1] = linalg.expm(-(1j) * hamiltonian(seq_f[k]) * dt).dot(psi[:,k])
+        seq = seq_f
+    fid = (np.absolute(psi[-1,-1]))**2
+    pseudo[:,-1] = observable.dot(psi[:,-1])
+
+    if episode > ep_max-11:
+        if fid>fid_max:
+            fid_max = np.copy(fid)
+    
+print('Final_fidelity=',fid_max)
+
+
+
+
+
+
+
+
+    
---
+++ multiqubit/sgd.py
@@ -0,0 +1,62 @@
+import numpy as np
+from scipy.linalg import expm
+
+
+def hamiltonian(j):
+    j=j.tolist()
+    dim=len(j)
+    J  = 1.
+
+    H  = np.diag([1. for ii in range(dim-1)],1)+np.diag([1. for ii in range(dim-1)],-1)
+    H += J*np.diag(j)
+    return H
+
+
+def cost(seq,T):
+    dim=len(seq[0])     
+    dt = T/len(seq)
+    U = np.matrix(np.identity(dim, dtype=complex)) 
+
+    for ii in seq:
+        H = hamiltonian(ii)
+        U = expm(-1j * H * dt) * U  # Evolution operator
+
+    p0    = np.zeros([dim,1])
+    p0[0] = 1
+    pt    = U * p0              #final state
+    
+    target     = np.zeros([dim,1])
+    target[-1] = 1
+    
+
+    err = 1-(np.abs(pt.H * target)**2).item(0).real            
+    return err
+
+def gradient_descent(x, learning_rate, T, num_iterations):
+    delta=0.01
+    for i in range(num_iterations):
+        v=np.random.random(np.shape(x))
+        xp=x+v*delta
+        xm=x-v*delta
+        error_derivative = (cost(xp,T) - cost(xm,T))/(2*delta)
+        x = x - (learning_rate) * error_derivative*v
+
+    return 1 - cost(x,T)
+
+
+dim=8
+N = 20
+T = np.pi*(dim-1)        
+ep_max=500
+
+seq = 40*np.random.rand(N,dim)
+fidelity= gradient_descent(seq, 0.01, T, ep_max)
+
+print ('Final_fidelity=',fidelity)
+
+
+
+
+
+
+
