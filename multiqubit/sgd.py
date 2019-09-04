1f07f67f8777e5a9d011aa8ba739e9109cfa00fe
93xiaoming <39090764+93xiaoming@users.noreply.github.com>
2019-09-04 10:57:43 +0800

    Update sgd.py

--- multiqubit/sgd.py
+++ multiqubit/sgd.py
@@ -5,10 +5,9 @@ from scipy.linalg import expm
 def hamiltonian(j):
     j=j.tolist()
     dim=len(j)
-    J  = 1.
-
-    H  = np.diag([1. for ii in range(dim-1)],1)+np.diag([1. for ii in range(dim-1)],-1)
-    H += 0.5*J*np.diag(j) #The coupling should be (effectively) divided by a factor of 2 
+    J  = 2*1. #should (effectively) multiply by a factor of 2 
+    H  = np.diag([J for ii in range(dim-1)],1)+np.diag([J for ii in range(dim-1)],-1)
+    H += J*np.diag(j) 
     return H
 
 
